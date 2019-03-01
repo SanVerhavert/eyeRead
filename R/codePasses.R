6735ff22c4de08656c39619f8dded3e318b6200d
Verhavert San <SVerhavert@ad.ua.ac.be>
2019-03-01 12:06:06 +0100

    updated documentation of codePasses

--- R/codePasses.R
+++ R/codePasses.R
@@ -21,10 +21,12 @@
 #' @param fpy The name of the column containing the Y coordinate of the fixation 
 #'   point. Required if \code{Rereading} 
 #'   is \code{TRUE}.
+#' @param fix_min [optional] minimal number of fixations for first pass. See Details.
+#'   Default is 3.
 #' 
 #' @details This function takes a data frame containing information of an eye 
-#'   tracking experiment. Each row indicated a fixation and the columns at least 
-#'   indicate and AOI that was fixated or if a specific AOI was fixated or not. 
+#'   tracking reading exersise Each row indicates a fixation and the columns at 
+#'   least indicate the AOI that was fixated or if a specific AOI was fixated or not. 
 #'   Optionally it can indicate the X and Y coordinates of the fixation point.  
 #'   The fixations in this dataset are then coded according to their pass status: 
 #'   first pass and second pass.
@@ -41,15 +43,21 @@
 #'   of the columns containing the x and y coordinates of the fixation point 
 #'   should be suplied by \code{fpx} and \code{fpy} respectively. The unit of 
 #'   these coordinates does not matter.
+#'   
+#'   By default this function considers the first three (3) fixations in any AOI 
+#'   as first pass fixations. And it does this regardless of whether the fixations
+#'   are consecutive or interupted by fixations in a different AOI. The minimal 
+#'   number of fixations considered as first pass can be changed through 
+#'   \code{fix_min}.
 #' 
 #' @return The function returns a character vector of the same length as the 
 #'   number of rows in data. Depending on the respecitve settings it contains the 
 #'   following values with their respective meanings.  
 #'   
 #'   * \code{rereading} is \code{FALSE}:
-#'      + \code{SP_\#} Fist Pass
-#'      + \code{2P_\#} Second Pass
-#'   * \code{repreding} is \code{TRUE}
+#'      + \code{FP_\#} First Pass
+#'      + \code{SP_\#} Second Pass
+#'   * \code{rereading} is \code{TRUE}
 #'      + \code{FPF_\#} First Pass Forward
 #'      + \code{FPR_\#} First Pass Rereading
 #'      + \code{SP_\#} Second Pass
