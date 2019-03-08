aa5fbbf615ed32abc7b888727d7d7791abf2243f
Verhavert San <SVerhavert@ad.ua.ac.be>
2019-03-08 13:35:55 +0100

    updated, renamed and split up test helper files, updated test files for codePasses

--- tests/testthat/test_N_codePasses.R
+++ tests/testthat/test_N_codePasses.R
@@ -6,49 +6,49 @@ test_that( "specFile is found", {
 
 test_that( "Function returns the correct error when data is no data frame", {
   expect_error(
-    codePasses( data = codePasses_Data$regular$single_AOI_col$AOI, AOI = "AOI"),
-    regexp = codePasses_results$no_data.frame_err
+    codePasses( data = some_Data$single_AOI_col$AOI, AOI = "AOI"),
+    regexp = some_errors$no_data.frame_err
   )
 } )
 
 test_that( "Function returns the correct error when data is no data frame", {
   expect_error(
-    codePasses( data = codePasses_Data$regular$single_AOI_col, AOI = "ABC"),
-    regexp = codePasses_results$no_colname_err
+    codePasses( data = some_Data$single_AOI_col, AOI = "ABC"),
+    regexp = some_results$no_colname_err
   )
 } )
 
 test_that( "Function returns the correct errors when fix_min is to small", {
   expect_error(
-    codePasses( data = codePasses_Data$regular$single_AOI_col, AOI = "AOI",
+    codePasses( data = some_Data$single_AOI_col, AOI = "AOI",
                 fix_min = 0 ),
-    regexp = codePasses_results$fix_min_err
+    regexp = some_results$fix_min_err
   )
   expect_error(
-    codePasses( data = codePasses_Data$regular$single_AOI_col, AOI = "AOI",
+    codePasses( data = some_Data$single_AOI_col, AOI = "AOI",
                 fix_min = -1 ),
-    regexp = codePasses_results$fix_min_err
+    regexp = some_results$fix_min_err
   )
 } )
 
 test_that( "Function returns correct error if rereading is TRUE but fpx and/or fpy are not supplied", {
   expect_error(
-    codePasses( data = codePasses_Data$regular$single_AOI_col,
+    codePasses( data = some_Data$single_AOI_col,
                 AOI = "AOI", rereading = T ),
-    regexp = codePasses_results$missing_fpx_fpy_err
+    regexp = some_results$missing_fpx_fpy_err
   )
 } )
 
 test_that( "Function correctly codes fixations as first pass and second pass with a single AOI column [column name]", {
   expect_equal(
-    codePasses( data = codePasses_Data$regular$single_AOI_col, AOI = "AOI" ),
-    codePasses_results$regular
+    codePasses( data = some_Data$single_AOI_col, AOI = "AOI" ),
+    some_results$regular
   )
 } )
 
 test_that( "Function correctly codes fixations as first pass and second pass with a single AOI column [column number]", {
   expect_equal(
-    codePasses( data = codePasses_Data$regular$single_AOI_col, AOI = 2 ),
-    codePasses_results$regular
+    codePasses( data = some_Data$single_AOI_col, AOI = 2 ),
+    some_results$regular
   )
 } )
