6ac146e9535ca7c10b3ab6f94eb0767165fe63ad
San Verhavert <SVerhavert@ad.ua.ac.be>
2020-07-07 13:54:27 +0200

    Created a simulated dataset for demonstration purposes and a preliminary for documentation

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' A simulated dataset with eye tracking data
+#' 
+#' This data set contains simulated eye tracking data for demonstration purposes.  
+#' It has the correct data structure for use with the package eyeRead. Mind that 
+#' it also contains expected results from functions.  
+#' 
+#' @format A data set with 37 rows and 8 variables
+#' /describe{
+#'   /item{fixationIndex}{a unique numerical index for each fixation}
+#'   /item{AOI1}{1 if AOI1 was fixated at that time and 0 otherwise}
+#'   /item{AOI2}{1 if AOI2 was fixated at that time and 0 otherwise}
+#'   /item{AOI3}{1 if AOI3 was fixated at that time and 0 otherwise}
+#'   /item{AOI}{the name of the AOI that was fixated at that time; expected outcome of codePasses}
+#'   /item{xcoord}{the x coordinate of the fixation position; origin top left}
+#'   /item{ycoord}{the y coordinate of the fixation position; origin top left}
+#'   /item{fixTime}{the time duration that the AOI was fixated}
+#'   /item{passes}{the result of codePasses with rereading = TRUE}
+#'   }
+#'   
+
+"SimData"
